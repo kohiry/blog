861f3f2dcb245f158a48ae5c5ad43289acbf0af5
maxik <kohiry@mail.ru>
2024-10-26 15:46:54 +0300

    [+] upd abstract class

--- app/pkg/common/repository.py
+++ app/pkg/common/repository.py
@@ -7,13 +7,23 @@ __all__ = [
     "BaseRepository",
 ]
 
+from app.pkg.common.model import BaseModel
+
 
 class BaseRepository(metaclass=ABCMeta):
     """Abstract class of pattern repository."""
 
     @abstractmethod
-    def get_by_name(self, query: BaseSchema, session: AsyncSession) -> BaseSchema:
+    def get_by_id(self, query: BaseSchema, session: AsyncSession) -> BaseSchema:
         """Abstract method for getting by name."""
+        pass
+
+    @abstractmethod
+    def _check_exist_by_id(
+        self,
+        query: BaseSchema,
+        session: AsyncSession,
+    ) -> BaseModel | None:
         pass
 
     @abstractmethod
@@ -22,11 +32,11 @@ class BaseRepository(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def delete_by_name(self, cmd: BaseSchema, session: AsyncSession) -> BaseSchema:
+    def delete_by_id(self, cmd: BaseSchema, session: AsyncSession) -> BaseSchema:
         """Abstract method for delete entity."""
         pass
 
     @abstractmethod
-    def update(self, cmd: BaseSchema, session: AsyncSession) -> BaseSchema:
+    def update_by_id(self, cmd: BaseSchema, session: AsyncSession) -> BaseSchema:
         """Abstract method for update entity."""
         pass
