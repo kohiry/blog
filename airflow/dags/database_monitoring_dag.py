30d2e7e18d9863b2ec4e43a5fe1622648223877c
maxik <kohiry@mail.ru>
2024-11-03 23:25:25 +0300

    [+] add transform task

--- airflow/dags/database_monitoring_dag.py
+++ airflow/dags/database_monitoring_dag.py
@@ -1,12 +1,30 @@
+import os
+
+import requests
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime, timedelta
 
+TOKEN, CHAT_ID = os.environ['TOKEN'], os.environ['CHAT_ID']
+
+def transform_data(ti):
+    query_result = ti.xcom_pull(task_ids='extract_data')
+    query_result.sort()
+    msg = "Most popular posts here with date {{ ds }}: \n" + '\n'.join(
+        [
+            f"title={query[0]} content={query[1]} views={query[2]}"
+            for query in query_result
+        ]
+    )
+    ti.xcom_push(key='query_result', value=msg)
+
 
 def send_telegram_message(ti):
-    query_result = ti.xcom_pull(task_ids='extract_data')
-    print(query_result)
+    query_result = ti.xcom_pull(task_ids='transform_data')
+    url = (f"https://api.telegram.org/bot{TOKEN}"
+           f"/sendMessage?chat_id={CHAT_ID}&text={query_result}")
+    requests.get(url)
     return 'end'
 
 with DAG(
@@ -26,17 +44,23 @@ with DAG(
         task_id='extract_data',
         postgres_conn_id='postgres',
         sql="""
-            SELECT views
+            SELECT title, content, views
             FROM posts 
             WHERE views > 1 and updated_at >= '{{ ds }}'
         """,
         do_xcom_push=True,
     )
 
+    transform_data = PythonOperator(
+        task_id='transform_data',
+        python_callable=transform_data,
+        trigger_rule='all',
+    )
+
     send_notification = PythonOperator(
         task_id='send_notification',
         python_callable=send_telegram_message,
-        trigger_rule='one_success',
+        trigger_rule='all',
     )
 
-    extract_data >> [send_notification]
+    extract_data >> transform_data >> [send_notification]
